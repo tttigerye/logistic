74b9f1e630c745875e58e624da7b9138ab6df450
Tiger <tigerye@umich.edu>
2023-11-17 01:01:11 -0500

    add more discription for thius function.

--- R/Logistic_Function.R
+++ R/Logistic_Function.R
@@ -1,3 +1,22 @@
+#' Logistic Function
+#' Computes the logistic function, which is an S-shaped curve.
+#' The logistic function is commonly used in logistic regression and
+#' returns values between 0 and 1.
+#'
+#' @param z A numeric vector or value.
+#'
+#' @return Computed logistic function values.
+#' @export
+#'
+#' @examples
+#' logistic_function(0)   # Returns 0.5
+#' logistic_function(2)   # Returns a value > 0.5
+#' logistic_function(-2)  # Returns a value < 0.5
+#'
+#' # Vectorized input
+#' z_values <- c(-2, 0, 2)
+#' logistic_function(z_values)  # Apply logistic function to each element
+#'
 logistic_function <- function(z) {
   return(1 / (1 + exp(-z)))
 }
