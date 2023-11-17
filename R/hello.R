0bf7f7ec28d7f6f22ad0f97f6bfb212e4788fc7c
Tiger <tigerye@umich.edu>
2023-11-16 22:58:57 -0500

    make some comment change

--- R/hello.R
+++ R/hello.R
@@ -3,7 +3,8 @@
 #' This function prints a greeting message. It's a simple demonstration of a function
 #' that takes no arguments and returns no value, but performs an action (printing a message).
 #'
-#' @param x Unused parameter.
+#' @param x any parameter here.
+#' @return "Hello, this is my function"
 #'
 #' @examples
 #' hello() # prints "Hello, this is my function"
