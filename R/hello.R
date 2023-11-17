865bd9ed0aeae71842658f03c4d6fd417eb8441f
Tiger <tigerye@umich.edu>
2023-11-16 21:43:39 -0500

    make some change for understanding github

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,14 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
+#' Hello Function
+#'
+#' This function prints a greeting message. It's a simple demonstration of a function
+#' that takes no arguments and returns no value, but performs an action (printing a message).
+#'
+#' @param x Unused parameter.
+#'
+#' @examples
+#' hello() # prints "Hello, this is my function"
+#'
+#' @export
 hello <- function(x) {
   print('Hello, this is my function')
 }
