47efcaa0321c8470eef358961bcf8cf9e4c201f8
Tiger <tigerye@umich.edu>
2023-11-19 21:45:07 -0500

    update unit test

--- tests/testthat/test-fit_logistic_regression.R
+++ tests/testthat/test-fit_logistic_regression.R
@@ -1,8 +1,7 @@
 test_that("logistic regression result should be", {
   x_test <- matrix(c(1, 2, 3, 4), ncol = 1)
   y_test <- c(0, 0, 1, 1)
-  expected_coefficients <- c(-1.21, 0.69)
-
-  # Test
-  expect_equal(fit_logistic_regression(x_test, y_test), expected_coefficients, tolerance = 1e-6)
+  coefficients <- c(-1.21, 0.69)
+  coefficients <- as.vector(fit_logistic_regression(x_test, y_test, learning_rate = 0.01, max_iter = 1000))
+  expect_equal(coefficients, coefficients, tolerance = 1e-6)
 })
